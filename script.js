90087634272d7f96e81204ebba9a51c362012d0d
Julie J <jeannet.julie@gmail.com>
2022-07-11 10:27:34 +0200

    Détection window.innerWidth

--- script.js
+++ script.js
@@ -13,13 +13,20 @@ function showMore() {
 function burger() {
   var x = document.getElementById("nav_items");
   var swith_nav = document.getElementById("nav_toggler");
+  var w = window.innerWidth;
+
   swith_nav.classList.toggle("fa-xmark")
   if (x.style.display === "flex") {
     x.style.display = "none";
-  } else {
-    x.style.display = "flex";
-  }
-  
-}
+  } else if(x.style.display="none"){
+    x.style.display="flex"
+  };
+  }  
 
+
+function myFunction() {
+  var x = document.getElementById("nav_items");
+  var w = window.innerWidth;
+  if(w>768){x.style.display = "flex"}else{x.style.display ="none"};
+}
 
